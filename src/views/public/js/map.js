d4db44cb5f66888865bcec66cc589c49ce8a73a1
LucaHaverty <lucahaverty@gmail.com>
2024-01-19 10:14:20 -0800

    screen percentage and showing building test

--- src/views/public/js/map.js
+++ src/views/public/js/map.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const map = document.getElementById('map');
     const mapContent = document.getElementById('map-content');
+    const testBuilding = document.getElementById('test-building');
+
     let isDragging = false;
     let offset = { x: 0, y: 0 };
 
@@ -45,7 +47,6 @@ document.addEventListener('DOMContentLoaded', function() {
     
         const zoomSpeed = 0.1;
     
-        const containerRect = map.getBoundingClientRect();
         const mouseX = e.clientX - window.innerWidth/2;
         const mouseY = e.clientY - window.innerHeight/2;
     
@@ -69,5 +70,48 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function updateTransform() {
         map.style.transform = `translate(${currentTransform.x}px, ${currentTransform.y}px) scale(${currentTransform.scale})`;
+        updateBuildings();
     }
+
+    function updateBuildings() {
+        const area = calcScreenPercentage(testBuilding);
+        if (area > 0.1)
+            testBuilding.style.backgroundColor = '#ff0000';
+        else 
+            testBuilding.style.backgroundColor = '#000000';
+    }
+
+    // percentage of the screen taken up by a div
+    function calcScreenPercentage(divElement) {
+        // screen dimensions
+        let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+        let viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+        // kinda scuffed but seems to work so not gonna fuck with it
+
+        let rect = divElement.getBoundingClientRect();
+
+        let x1 = rect.x;
+        let y1 = rect.y;
+
+        let x2 = x1 + rect.width;
+        let y2 = y1 + rect.height;
+
+        // clamp to be in screen view
+        x1 = clamp(x1, 0, viewportWidth);
+        y1 = clamp(y1, 0, viewportHeight);
+        x2 = clamp(x2, 0, viewportWidth);
+        y2 = clamp(y2, 0, viewportHeight);
+
+        let onScreenRectWidth = x2 - x1;
+        let onScreenRectHeight = y2 - y1;
+
+        // calc percentage
+        let areaPercentage = ((onScreenRectWidth * onScreenRectHeight) / (viewportWidth * viewportHeight));
+        return areaPercentage;
+    }
+
+    function clamp(num, min, max) {
+        return Math.min(Math.max(num, min), max);
+    };
 });
