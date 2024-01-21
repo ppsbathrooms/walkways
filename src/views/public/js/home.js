cc6e865d27a22ab4d13e59dc06bcb936ddb4dcc8
finnmprice <finnprice03@gmail.com>
2024-01-20 17:18:55 -0800

    user profile photo for settings

--- src/views/public/js/home.js
+++ src/views/public/js/home.js
@@ -1,16 +1,3 @@
 fetch("/api/brdata")
   .then((response) => response.json())
   .then((data) => $("#br-data").html(JSON.stringify(data)));
-
-const { isAuthenticated, user } = JSON.parse($('#data').html());
-
-if (isAuthenticated) {
-  $("#signInButton").hide();
-  $("#signOutButton").show();
-  if (user) {
-    $("#user-info").html(`${user.displayName} - ${user.email}`);
-  }
-} else {
-  $("#signInButton").show();
-  $("#signOutButton").hide();
-}
